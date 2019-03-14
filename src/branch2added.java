b878b164eb45b4fe93333cb89a18c2a98f56c2d2
wangdexi <wadexi2207@163.com>
2019-03-14 15:14:44 +0800

    branch1 commit1

---
+++ src/branch2added.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+/**
+ * 功能描述: // todo
+ *
+ * @PackageName PACKAGE_NAME
+ * @Autor wang dexiao
+ * @Date 2019/3/14 15:13
+ * @Version 1.0
+ **/
+public class branch2added {
+}
